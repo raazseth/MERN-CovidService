d947fba9fee2f9fa58b2e9f4d38d6d20e93d0199
Raj <payalseth33@gmail.com>
2021-04-26 00:25:22 +0530

    app env

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 const app = express();
 const path = require("path");
 // require("dotenv").config();
 const { ATLAS_URI } = require("./Config/keys");
 
-app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+if(process.env.NODE_ENV=="production"){
+  app.use(express.static('client/build'))
+  const path = require('path')
+  app.get("*",(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
 }
-
 // const uri = process.env.ATLAS_URI;
 const uri = ATLAS_URI;
 mongoose.connect(uri, {
